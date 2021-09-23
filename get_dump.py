a7c1d6a67b6e5fc8e947e805e585558cf7a60d7e
Tom Wang <tom.wang@badal.io>
2021-09-22 21:09:39 -0400

    insert couchbase local users to destination cluster

--- get_dump.py
+++ get_dump.py
@@ -5,6 +5,9 @@ from couchbase_core.cluster import PasswordAuthenticator
 from couchbase.management.users import UserManager
 from couchbase.management.buckets import BucketManager
 from couchbase.management.admin import Admin
+from couchbase.management.users import Role, User, UpsertUserOptions
+from couchbase.auth import AuthDomain
+import json
 
 
 cluster_flm_url = "couchbase://10.119.16.48"
@@ -26,15 +29,6 @@ admin = Admin('Administrator', 'password', my_cb_ip)
 bucket_mgr = BucketManager(admin)
 buckets = bucket_mgr.get_all_buckets()
 
-# try:
-#     print(buckets)
-
-# except:
-#   print("exception:", sys.exc_info()[0])
-
-
-#c_mger = ClusterManager('couchbase://34.93.38.177', 'Administrator', 'password')
-
 src_user_mgr = UserManager(admin)
 src_users = src_user_mgr.get_all_users()
 
@@ -50,7 +44,21 @@ print("users are:\n")
 for user_meta in src_users:
   print(user_meta, "\n")
   print(user_meta.user.username)
-  dest_user_mgr.upsert_user(user_meta.user)
+  
+  # print python obj to json
+  print(json.dumps(user_meta.raw_data, indent = 4))
+  
+  for role_origin in user_meta.effective_roles:
+    print(role_origin.role.to_server_str())
+    print(role_origin.role.name, role_origin.role.bucket, '\n')
+
+    dest_role_list = [ Role(name=role_origin.role.name, bucket=role_origin.role.bucket) ]
+
+    # insert that user in destination cluster
+    dest_user_mgr.upsert_user(
+                              User(username=user_meta.user.username, roles=dest_role_list, password="password"),
+                              UpsertUserOptions(domain_name="local")
+    )
 
 
 
