c3d2763670f884ef732b30720bbe9c0a1e891eb4
Tom Wang <tom.wang@badal.io>
2021-09-28 18:01:09 -0400

    add steps to cleanup backup dir. and backup bucket each run of script

--- backup_no_data.sh
+++ backup_no_data.sh
@@ -6,6 +6,7 @@ set -e
 
 export PATH=/opt/couchbase/bin:$PATH
 
+rm -rf /data/backup_migrate
 mkdir /data/backup_migrate
 
 # config backup folder
@@ -14,5 +15,7 @@ cbbackupmgr config -a /data/backup_migrate -r cluster --disable-data
 # backup the cluster
 cbbackupmgr backup -c 127.0.0.1 -u $CB_REST_USERNAME -p $CB_REST_PASSWORD -a /data/backup_migrate -r cluster
 
-# upload backup
+# remove older backup, and upload the new one
+# put a text file in first run so rm command won't error out
+gsutil rm gs://cb_backup/**
 gsutil -m cp -r /data/backup_migrate/cluster gs://cb_backup
