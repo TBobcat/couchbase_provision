628414a47b4c7e3268a20f84c2bd50ce1fbc6ddb
Tom Wang <tom.wang@badal.io>
2021-09-29 11:37:30 -0400

    modify backup dir and factor out credentials

--- backup_no_data.sh
+++ backup_no_data.sh
@@ -6,16 +6,16 @@ set -e
 
 export PATH=/opt/couchbase/bin:$PATH
 
-rm -rf /data/backup_migrate
-mkdir /data/backup_migrate
+rm -rf /tmp/backup_migrate
+mkdir /tmp/backup_migrate
 
 # config backup folder
-cbbackupmgr config -a /data/backup_migrate -r cluster --disable-data
+cbbackupmgr config -a /tmp/backup_migrate -r cluster --disable-data
 
 # backup the cluster
-cbbackupmgr backup -c 127.0.0.1 -u $CB_REST_USERNAME -p $CB_REST_PASSWORD -a /data/backup_migrate -r cluster
+cbbackupmgr backup -c 127.0.0.1 -u $CB_REST_USERNAME -p $CB_REST_PASSWORD -a /tmp/backup_migrate -r cluster
 
 # remove older backup, and upload the new one
 # put a text file in first run so rm command won't error out
 gsutil rm gs://cb_backup/**
-gsutil -m cp -r /data/backup_migrate/cluster gs://cb_backup
+gsutil -m cp -r /tmp/backup_migrate/cluster gs://cb_backup
--- restore_no_data.sh
+++ restore_no_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 # run script as a sudoer
 # script assumes there's only one backup in the gs bucket and restores from it
+# script nees cred.sh sourced to get credentials from exported environment variable
 
 set -e
 
@@ -10,7 +11,7 @@ mkdir /data/backup_migrate
 # config backup folder and bakup repo for couchbase
 cbbackupmgr config -a /data/backup_migrate -r cluster --disable-data
 
-gsutil cp -m -r gs://cb_backup/cluster/* /data/backup_migrate/cluster/
+gsutil -m cp -r gs://cb_backup/cluster/* /data/backup_migrate/cluster/
 
 cbbackupmgr restore -a /data/backup_migrate  -r cluster -c http://127.0.0.1:8091 \
-    -u Administrator -p password  --auto-create-buckets
+    -u $CB_REST_USERNAME -p $CB_REST_PASSWORD  --auto-create-buckets
