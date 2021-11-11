44e9b8abc155d4619f29c6131f98e8d15acac6a9
Tom Wang <tom.wang@badal.io>
2021-11-10 22:59:30 -0500

    add python package requirements, clean up vars.py

--- vars.py
+++ vars.py
@@ -1,19 +1,14 @@
 
-# my source cluster ip
-# source_ip = '35.200.175.198'
-# admin = ''
-# pwd = ''
-
-
-
-
 ## client staging cluster credentials, and ip
 admin = ''
 pwd = ''
-source_ip = '10.119.16.48'
+source_ip = ''
 
 
 # put in ip of data-1 of dest. cluster, find that through name of VM in GCP console
-dest_ip = '35.200.175.198'
+dest_ip = ''
 source_url = 'couchbase://' + source_ip
+
+
+
 
